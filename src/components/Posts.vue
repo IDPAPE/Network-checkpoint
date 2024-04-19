334a121aea0536b39032e6536005c3e70c05aa17
isaiah <isaiahpape@gmail.com>
2024-04-19 11:21:54 -0600

    first commit

---
+++ src/components/Posts.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+
+<template>
+<div class="row bg-primary p-4">
+    <form class="text-light">
+        <div class="col-12">
+            <h4>Post Body</h4>
+            <textarea class="w-100" name="post-body" id="post-body" cols="" rows="10"></textarea>
+        </div>
+        <div class="container-fluid">
+            <div class="row w-100">
+                <div class="col-4">
+                    <input class="w-100 h-100 rounded" type="text" placeholder="insert image URL">
+                </div>
+                <div class="col-2">
+                    <button class="btn btn-light w-100">post</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/ProfileDisplay.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+
+<template>
+        <div class="row">
+            <div class="col">
+                <div class="row justify-content-center">
+                    <img class="pfp"
+                        src="https://imgs.search.brave.com/BXqOJuF_RVacrwxjODkF2cNrHuZKn8-Ofy593M31LmM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMubmlnaHRjYWZl/LnN0dWRpby8vYXNz/ZXRzL21hbi1pbi1z/dWl0LmpwZz90cj13/LTE2MDAsYy1hdF9t/YXg"
+                        alt="PFP">
+                </div>
+                <p class="m-0">Class</p>
+                <h4>UserName</h4>
+                <h5>Linkedin</h5>
+                <h5>GitHub</h5>
+                <h5>Resume</h5>
+            </div>
+        </div>
+</template>
+
+
+<style lang="scss" scoped>
+    .pfp{
+  height: 100px;
+  width: 100px;
+  object-fit: contain;
+  object-position: center;
+}
+</style>
